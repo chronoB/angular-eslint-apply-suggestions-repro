363f7a3a8a444ea7b21257609c8f90a083fdaedd
Finn Bayer <finn.o.bayer@gmail.com>
2026-03-29 20:57:25 +0200

    add linting error

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'wrongSelector-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
